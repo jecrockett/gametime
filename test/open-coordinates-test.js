a94dc95f04522fa5c33827fd703714c317d97918
Justin Pease <justinpease2@gmail.com>
2016-04-06 14:57:54 -0600

    adds test for coordGenerator making coords outside players

--- test/open-coordinates-test.js
+++ test/open-coordinates-test.js
@@ -1,14 +1,28 @@
-const chai = require('chai');
+const chai   = require('chai');
 const assert = chai.assert;
 
-const Player = require('../public/online-player');
-const FoodGenerator = require('../lib/food-generator');
+const Player          = require('../public/online-player');
+const OpenCoordinates = require('../lib/open-coordinates');
 
 describe('OpenCoordinates', function(){
-  it('creates food outside all players', function(){
-    var canvas = { width: 500, height: 400 };
-    var foodGen = new FoodGenerator(canvas.width, canvas.height);
+  context('creates coordinates', function(){
+    it('outside all players', function(){
+      var canvas = { width: 500, height: 400 };
+      var player = new Player(1, "J", 5, 5, "black");
+      var playerCoords = { x: 5, y: 5};
+      var players = [player];
+      var openCoordMaker = new OpenCoordinates(canvas.width, canvas.height);
+      var coords = [];
+      for(var i = 0; i < 200; i++) {
+        var openCoords = openCoordMaker.create(players);
+        coords.push(openCoords);
+      }
+      console.log(coords);
+      console.log(player);
 
-    assert.equal(foodGen.canvasWidth, 500);
-    assert.equal(foodGen.canvasHeight, 400);
+      coords.forEach(function(singleCoord){
+        assert.notEqual(singleCoord, playerCoords);
+      });
+    });
   });
+});

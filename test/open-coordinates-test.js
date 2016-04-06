62f2a41767b7cb7273a661694da693c6edb27a0d
Justin Pease <justinpease2@gmail.com>
2016-04-06 15:00:22 -0600

    adds second player to test against and removes console log

--- test/open-coordinates-test.js
+++ test/open-coordinates-test.js
@@ -9,19 +9,20 @@ describe('OpenCoordinates', function(){
     it('outside all players', function(){
       var canvas = { width: 500, height: 400 };
       var player = new Player(1, "J", 5, 5, "black");
-      var playerCoords = { x: 5, y: 5};
-      var players = [player];
+      var player2 = new Player(2, "k", 345, 255, "white");
+      var playerCoords = { x: player.x, y: player.y};
+      var player2Coords = { x: player2.x, y: player2.y};
+      var players = [player, player2];
       var openCoordMaker = new OpenCoordinates(canvas.width, canvas.height);
       var coords = [];
       for(var i = 0; i < 200; i++) {
         var openCoords = openCoordMaker.create(players);
         coords.push(openCoords);
       }
-      console.log(coords);
-      console.log(player);
 
       coords.forEach(function(singleCoord){
         assert.notEqual(singleCoord, playerCoords);
+        assert.notEqual(singleCoord, player2Coords);
       });
     });
   });

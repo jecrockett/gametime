5b224aae53ef1e69c08a9664911a097899860293
Joe Perry <joseph.w.perry@gmail.com>
2016-04-07 13:46:08 -0600

    Additional virus movement testing

--- test/virus-test.js
+++ test/virus-test.js
@@ -4,23 +4,43 @@ const assert = chai.assert;
 const Virus = require('../lib/virus');
 
 describe('Virus', function(){
-  it('should have an x property', function(){
-    let virus = new Virus({x: 40, y: 300});
-    assert.equal(virus.x, 40);
+  context('properties', function(){
+    it('should have an x property', function(){
+      let virus = new Virus({x: 40, y: 300});
+      assert.equal(virus.x, 40);
+    });
+
+    it('should have a y property', function(){
+      let virus = new Virus({x: 40, y: 300});
+      assert.equal(virus.y, 300);
+    });
+
+    it('should have a default mass', function(){
+      let virus = new Virus({x: 40, y: 300});
+      assert.isAbove(virus.mass, 0);
+    });
+
+    it('should be red by default', function(){
+      let virus = new Virus({x: 40, y: 300});
+      assert.equal(virus.color, 'red');
+    });
   });
 
-  it('should have a y property', function(){
-    let virus = new Virus({x: 40, y: 300});
-    assert.equal(virus.y, 300);
-  });
+  context('movement', function(){
+    it('moves the first virus towards the largest player (positive)', function(){
+      let virus = new Virus({x:100, y:100});
+      let testPlayer = {x: 150, y: 150};
+      virus.moveVirus(testPlayer);
+      assert.isAbove(virus.x, 100);
+      assert.isAbove(virus.y, 100);
+    });
 
-  it('should have a default mass', function(){
-    let virus = new Virus({x: 40, y: 300});
-    assert.isAbove(virus.mass, 0);
-  });
-
-  it('should be red by default', function(){
-    let virus = new Virus({x: 40, y: 300});
-    assert.equal(virus.color, 'red');
+    it('moves the first virus towards the largest player (negative)', function(){
+      let virus = new Virus({x:100, y:100});
+      let testPlayer = {x: 50, y: 50};
+      virus.moveVirus(testPlayer);
+      assert.isBelow(virus.x, 100);
+      assert.isBelow(virus.y, 100);
+    });
   });
 });

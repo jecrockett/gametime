0d680cd064b930895cdf9f4ec09d3f21e29a7024
James Crockett <jcrockett@patientslikeme.com>
2019-03-20 13:06:52 -0400

    Update webpack config, and do npm run build

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,20 @@
 module.exports = {
   entry: {
     main: "./lib/index.js",
-    test: "mocha!./test/index.js"
+    test: "mocha-loader!./test/index.js"
   },
   output: {
     path: __dirname + '/public',
     filename: "[name].bundle.js"
   },
   module: {
-    loaders: [
+    rules: [
       { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader' },
       { test: /\.css$/, loader: "style!css" },
       { test: /\.scss$/, loader: "style!css!sass" }
     ]
   },
   resolve: {
-    extensions: ['', '.js', '.json', '.scss', '.css']
+    extensions: ['.js', '.json', '.scss', '.css']
   }
 };
